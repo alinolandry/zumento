c605be359973c1650b458fc6af0f91ca675511ce
Alain Landry Noutchomwo <alinolandry@gmail.com>
2021-09-16 00:05:39 -0400

    make some custom in the controller

--- OrderExport/Controller/Adminhtml/Export/Run.php
+++ OrderExport/Controller/Adminhtml/Export/Run.php
@@ -5,6 +5,7 @@ namespace Zumento\OrderExport\Controller\Adminhtml\Export;
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\Controller\Result\JsonFactory;
+use Zumento\OrderExport\Model\HeaderDataFactory;
 
 
 class Run extends Action
@@ -21,14 +22,26 @@ class Run extends Action
      */
     private $jsonFactory;
 
-     public function __construct(JsonFactory $jsonFactory, Context $context)
-     {
+    /**
+     * @var HeaderDataFactory
+     */
+    private $headerDataFactory;
+
+    public function __construct(JsonFactory $jsonFactory,
+                                 Context $context,
+                                 HeaderDataFactory $headerDataFactory
+     ) {
          parent::__construct($context);
          $this->jsonFactory = $jsonFactory;
-     }
+         $this->headerDataFactory = $headerDataFactory;
+    }
 
     public function execute()
     {
+        $headerData = $this->headerDataFactory->create();
+        $headerData->setShipDate(new \DateTime($this->getRequest()->getParam("ship_date")));
+        $headerData->setMerchantNotes(htmlspecialchars($this->getRequest()->getParam("merchant_notes")));
+
         return $this->jsonFactory->create([]);
     }
 }
